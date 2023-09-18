4092f8d60dfcddc89a4fc36525e8c49ddd89b02f
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-09-18 20:38:28 +0900

    #23 - 간단한 수정사항, 그 전에 놓친 부분들

--- src/main/java/com/example/projectboard/domain/type/SearchType.java
+++ src/main/java/com/example/projectboard/domain/type/SearchType.java
@@ -5,12 +5,11 @@ import lombok.Getter;
 public enum SearchType {
     TITLE("제목"),
     CONTENT("본문"),
-    ID("유저ID"),
+    ID("유저 ID"),
     NICKNAME("닉네임"),
     HASHTAG("해시태그");
-    
-    @Getter
-    private final String description;
+
+    @Getter private final String description;
 
     SearchType(String description) {
         this.description = description;
